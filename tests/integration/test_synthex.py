87d02b3737d9f88f37f92732686e9df4a5cd83f8
rlucatoor <rlucato95@gmail.com>
2025-04-01 13:04:40 +0200

    add test for synthex.users.me

--- tests/integration/test_synthex.py
+++ tests/integration/test_synthex.py
@@ -1,6 +1,7 @@
 import pytest
 
 from synthex import Synthex
+from synthex.models import UserResponseModel
 
 
 @pytest.mark.integration
@@ -14,3 +15,20 @@ def test_ping(synthex: Synthex):
     """
     
     assert synthex.ping() is True
+    
+    
+@pytest.mark.integration
+def test_get_current_user(synthex: Synthex):
+    """
+    Test the me method of the UsersAPI to ensure it retrieves user information.
+    Args:
+        synthex (Synthex): An instance of the Synthex class to be tested.
+    Asserts:
+        The get_current_user method of the UsersAPI returns a UserResponseModel object.
+    """
+    
+    user_info = synthex.users.me()
+    
+    print("user info: ", user_info)
+    
+    assert type(user_info) == UserResponseModel
