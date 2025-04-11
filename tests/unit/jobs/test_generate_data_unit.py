b4ccc5938df4fdc55299c235d2d45d3f7cb448ff
rlucatoor <rlucato95@gmail.com>
2025-04-11 10:08:57 +0200

    turn generate_data parameter validation tests from integration to unit

--- tests/unit/jobs/test_generate_data_unit.py
+++ tests/unit/jobs/test_generate_data_unit.py
@@ -2,10 +2,12 @@ import responses
 from typing import Any
 import os
 import csv
+import pytest
 
 from synthex import Synthex
 from synthex.endpoints import API_BASE_URL, CREATE_JOB_WITH_SAMPLES_ENDPOINT
 from synthex.config import OUTPUT_FILE_DEFAULT_NAME
+from synthex.exceptions import ValidationError
 
 
 json_body="data: [{\"question\": \"What is the enthalpy change for the combustion of 1 mole of methane?\",\
@@ -165,7 +167,9 @@ def test_generate_data_output_path_extensionless_file(synthex: Synthex, generate
 
 
 @responses.activate
-def test_generate_data_output_path_no_filename(synthex: Synthex, generate_data_params: dict[Any, Any]):
+def test_generate_data_output_path_no_filename(
+    synthex: Synthex, generate_data_params: dict[Any, Any]
+):
     """
     Test to ensure that, if the `output_path` parameter of the `generate_data` method of the `Synthex` class 
     specifies a path but not a file name, the function attaches the default file name with an extension that is
@@ -211,3 +215,183 @@ def test_generate_data_output_path_no_filename(synthex: Synthex, generate_data_p
     finally:   
         # Clean up the generated file after the test
         os.remove(correct_output_path)
+        
+
+def test_generate_data_schema_definition_wrong_type(
+    synthex: Synthex, generate_data_params: dict[Any, Any]
+):
+    """
+    Test case for validating the behavior of the `generate_data` method when an invalid 
+    `schema_definition` is provided. This test ensures that a `ValidationError` is raised when 
+    the `schema_definition` parameter has an incorrect type (e.g., an integer instead of 
+    the expected type).
+    Args:
+        synthex (Synthex): An instance of the Synthex class used to invoke the `generate_data` 
+            method.
+        generate_data_params (dict[Any, Any]): A dictionary containing the required parameters 
+    """
+
+    try:
+        with pytest.raises(ValidationError):
+            synthex.jobs.generate_data(
+                # Invalid argument type for schema_definition
+                schema_definition={
+                    "question": {
+                        "datatype": "string" #type: ignore
+                    },
+                    "option-a": {
+                        "type": "string"
+                    }
+                },
+                examples=generate_data_params["examples"],
+                requirements=generate_data_params["requirements"],
+                number_of_samples=generate_data_params["number_of_samples"],
+                output_type=generate_data_params["output_type"],
+                output_path=generate_data_params["output_path"]
+            )
+    except AssertionError:
+        pytest.fail("Expected ValidationError when an incorrect schema_definition is provided")
+
+
+def test_generate_data_examples_wrong_type(
+    synthex: Synthex, generate_data_params: dict[Any, Any]
+):
+    """
+    Test case for validating the behavior of the `generate_data` method when an invalid 
+    `examples` is provided. This test ensures that a `ValidationError` is raised when the 
+    `examples` parameter has an incorrect type (e.g., an integer instead of the 
+    expected type).
+    Args:
+        synthex (Synthex): An instance of the Synthex class used to invoke the `generate_data` 
+            method.
+        generate_data_params (dict[Any, Any]): A dictionary containing the required parameters 
+    """
+    
+    try:
+        with pytest.raises(ValidationError):
+            synthex.jobs.generate_data(
+                schema_definition=generate_data_params["schema_definition"],
+                # Invalid argument type for examples
+                examples=1, #type: ignore
+                requirements=generate_data_params["requirements"],
+                number_of_samples=generate_data_params["number_of_samples"],
+                output_type=generate_data_params["output_type"],
+                output_path=generate_data_params["output_path"]
+            )
+    except AssertionError:
+        pytest.fail("Expected ValidationError when an incorrect examples argument is provided")
+        
+
+def test_generate_data_requirements_wrong_type(
+    synthex: Synthex, generate_data_params: dict[Any, Any]
+):
+    """
+    Test case for validating the behavior of the `generate_data` method when an invalid 
+    `requirements` is provided. This test ensures that a `ValidationError` is raised when 
+    the `requirements` parameter has an incorrect type (e.g., an integer instead of the 
+    expected type).
+    Args:
+        synthex (Synthex): An instance of the Synthex class used to invoke the `generate_data` 
+            method.
+        generate_data_params (dict[Any, Any]): A dictionary containing the required parameters 
+    """
+    
+    try:
+        with pytest.raises(ValidationError):
+            synthex.jobs.generate_data(
+                schema_definition=generate_data_params["schema_definition"],
+                examples=generate_data_params["examples"],
+                # Invalid argument type for requirements
+                requirements=1, #type: ignore
+                number_of_samples=generate_data_params["number_of_samples"],
+                output_type=generate_data_params["output_type"],
+                output_path=generate_data_params["output_path"]
+            )
+    except AssertionError:
+        pytest.fail("Expected ValidationError when an incorrect requirements argument is provided")
+        
+
+def test_generate_data_num_of_samples_wrong_type(
+    synthex: Synthex, generate_data_params: dict[Any, Any]
+):
+    """
+    Test case for validating the behavior of the `generate_data` method when an invalid 
+    `number_of_samples` is provided. This test ensures that a `ValidationError` is raised when 
+    the `number_of_samples` parameter has an incorrect type (e.g., an integer instead of 
+    the expected type).
+    Args:
+        synthex (Synthex): An instance of the Synthex class used to invoke the `generate_data` method.
+        generate_data_params (dict[Any, Any]): A dictionary containing the required parameters 
+    """
+    
+    try:
+        with pytest.raises(ValidationError):
+            synthex.jobs.generate_data(
+                schema_definition=generate_data_params["schema_definition"],
+                examples=generate_data_params["examples"],
+                requirements=generate_data_params["requirements"],
+                # Invalid argument type for number_of_samples
+                number_of_samples=1001,
+                output_type=generate_data_params["output_type"],
+                output_path=generate_data_params["output_path"]
+            )
+    except AssertionError:
+        pytest.fail("Expected ValidationError when an incorrect number_of_samples argument \
+            is provided")
+        
+
+def test_generate_data_output_type_wrong_type(
+    synthex: Synthex, generate_data_params: dict[Any, Any]
+):
+    """
+    Test case for validating the behavior of the `generate_data` method when an invalid 
+    `output_type` is provided. This test ensures that a `ValidationError` is raised when the 
+    `output_type` parameter has an incorrect type (e.g., a string instead of the 
+    expected type).
+    Args:
+        synthex (Synthex): An instance of the Synthex class used to invoke the `generate_data` method.
+        generate_data_params (dict[Any, Any]): A dictionary containing the required parameters 
+    """
+    
+    try:
+        with pytest.raises(ValidationError):
+            synthex.jobs.generate_data(
+                schema_definition=generate_data_params["schema_definition"],
+                examples=generate_data_params["examples"],
+                requirements=generate_data_params["requirements"],
+                number_of_samples=generate_data_params["number_of_samples"],
+                # Invalid argument type for output_type
+                output_type="ttt", #type: ignore
+                output_path=generate_data_params["output_path"]
+            )
+    except AssertionError:
+        pytest.fail("Expected ValidationError when an incorrect output_type argument is provided")
+        
+
+def test_generate_data_output_path_wrong_type(
+    synthex: Synthex, generate_data_params: dict[Any, Any]
+):
+    """
+    Test case for validating the behavior of the `generate_data` method when an invalid 
+    `output_path` is provided. This test ensures that a `ValidationError` is raised when 
+    the `output_path` parameter has an incorrect type (e.g., an integer instead of 
+    the expected type).
+    Args:
+        synthex (Synthex): An instance of the Synthex class used to invoke the `generate_data` 
+            method.
+        generate_data_params (dict[Any, Any]): A dictionary containing the required parameters 
+    """
+    
+    try:
+        with pytest.raises(ValidationError):
+            synthex.jobs.generate_data(
+                schema_definition=generate_data_params["schema_definition"],
+                examples=generate_data_params["examples"],
+                requirements=generate_data_params["requirements"],
+                number_of_samples=generate_data_params["number_of_samples"],
+                output_type=generate_data_params["output_type"],
+                # Invalid argument type for output_type
+                output_path=1 #type: ignore
+            )
+    except AssertionError:
+        pytest.fail("Expected ValidationError when an incorrect output_path argument is provided")
